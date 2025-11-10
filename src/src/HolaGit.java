5fb2bdcef422b9f1a7827e1d2562782a06cfd96f
paunadal14-web <paunadal14@alumnes.ilerna.com>
2025-11-10 22:18:56 +0100

    Subimos HolaGit
    HolaGit

---
+++ src/src/HolaGit.java
@@ -0,0 +1,13 @@
+package src;
+
+public class HolaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Este es el mensaje que se mostrará en la consola
+        System.out.println("¡Hola Git!");
+		
+	}
+
+}
